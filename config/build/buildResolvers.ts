7080517a558c6fbb3715ca274d51a41deaeb1911
SlavaTikhonov <tikhonov.viache@gmail.com>
2023-02-05 18:42:16 +0200

    add config

---
+++ config/build/buildResolvers.ts
@@ -0,0 +1,9 @@
+import {ResolveOptions} from "dns";
+import webpack from "webpack";
+
+
+export function buildResolvers(): webpack.ResolveOptions{
+    return {
+        extensions: ['.tsx', '.ts', '.js'],
+    }
+}
