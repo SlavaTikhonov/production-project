7fc3e6db9eff1c0b9da58bbfc2fd838a83d70279
SlavaTikhonov <tikhonov.viache@gmail.com>
2023-02-19 20:34:38 +0200

    upgrade sidebar, add button themes

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,7 +5,6 @@ import { buildCssLoader } from './loaders/buildCssLoader';
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
         test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
         use: ['@svgr/webpack'],
     };
 
@@ -30,6 +29,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     };
 
     const cssLoader = buildCssLoader(isDev);
+
     // Ecли не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
         test: /\.tsx?$/,
--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -14,7 +14,6 @@ export function buildCssLoader(isDev: boolean) {
                             ? '[path][name]__[local]--[hash:base64:5]'
                             : '[hash:base64:8]',
                     },
-
                 },
             },
             'sass-loader',
