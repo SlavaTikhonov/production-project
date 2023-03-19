8dd2cff0cdf138fb5c62ca2c37b1a800ae0379de
SlavaTikhonov <tikhonov.viache@gmail.com>
2023-03-19 12:43:33 +0200

    add articles page

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,8 +1,8 @@
-import * as HtmlWebpackPlugin from 'html-webpack-plugin';
-import * as webpack from 'webpack';
-import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import * as ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
---
+++ src/pages/ArticleDetailsPage/index.ts
@@ -0,0 +1,3 @@
+export {
+    ArticleDetailsPageAsync as ArticlesPage,
+} from './ui/ArticleDetailsPage/ArticleDetailsPage.async';
