df9db787b254d1eb1a37df78742d9fca3f92cc8c
SlavaTikhonov <tikhonov.viache@gmail.com>
2023-03-22 22:58:08 +0200

    add comments list

--- src/pages/ArticleDetailsPage/index.ts
+++ src/pages/ArticleDetailsPage/index.ts
@@ -1,3 +1,5 @@
 export {
     ArticleDetailsPageAsync as ArticlesPage,
 } from './ui/ArticleDetailsPage/ArticleDetailsPage.async';
+
+export { ArticleDetailsCommentsSchema } from './model/types/ArticleDetailsCommentsSchema';
