3fa19f6e8c5160290d982e51dabf75d28b8af46b
SlavaTikhonov <tikhonov.viache@gmail.com>
2023-05-03 20:07:22 +0300

    fix storybook, add mocks for notifications list, article rating

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -22,6 +22,10 @@ export default ({ config }: {config: webpack.Configuration}) => {
     }
 
     config!.resolve!.extensions!.push('.ts', '.tsx');
+    config!.resolve!.alias = {
+        ...config!.resolve!.alias,
+        '@': paths.src,
+    };
 
     // eslint-disable-next-line no-param-reassign
     // @ts-ignore
