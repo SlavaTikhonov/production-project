19e0cc749299438ef51e577dc36987048f92e0ec
SlavaTikhonov <tikhonov.viache@gmail.com>
2023-09-01 12:19:32 +0300

    production

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -5,7 +5,12 @@ import { Page } from '@/widgets/Page';
 const MainPage = memo(() => {
   const { t } = useTranslation();
 
-  return <Page data-testid="MainPage">{t('Главная страница')}</Page>;
+  return (
+    <Page data-testid="MainPage">
+      <div>111111111</div>
+      {t('Главная страница')}
+    </Page>
+  );
 });
 
 export default MainPage;
